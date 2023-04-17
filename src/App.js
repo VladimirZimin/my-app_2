53bcca2a028267c6b30c6902e6f948b7f7b68d2b
VladimirZimin <zimin.frontend@gmail.com>
2023-04-17 12:16:02 +0300

    delete Task

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
 import Counter from './components/Counter/Counter';
 import Layout from './components/Layout/Layout';
-import TaskList from './components/TaskList/TaskList';
-import TaskEditor from './components/TaskEditor/TaskEditor';
-// import createTask from './utils/create-task';
 import Product from './components/Counter/Product';
 import Section from './utils/Section';
 import Dropdown from './components/Dropdown/Dropdown';
 import ColorPicker from './components/ColorPicker/ColorPicker';
 import SingupForm from './components/Form/SignupForm';
-import Filter from './components/Filter/Filter';
 import FormikForm from './components/FormikForm/FormikForm';
 import { ProductReviewForm } from './components/FormikForm/ProductReviewForm';
-import { GlobalStyle } from './components/GlobalStyle';
 
 const colorPickerOptions = [
   { label: 'red', color: '#F44336' },
@@ -29,34 +23,8 @@ const check = (balance, total) =>
 
 class App extends Component {
   state = {
-    tasks: [],
     wishes: [],
     balance: 0,
-    filter: '',
-  };
-
-  addTask = text => {
-    // const task = createTask();
-
-    const task = {
-      id: nanoid(),
-      text,
-      completed: false,
-    };
-
-    this.setState(prevState => {
-      return {
-        tasks: [...prevState.tasks, task],
-      };
-    });
-  };
-
-  removeTask = taskId => {
-    this.setState(prevState => {
-      return {
-        tasks: prevState.tasks.filter(task => task.id !== taskId),
-      };
-    });
   };
 
   buyProduct = (product, amount) => {
@@ -73,69 +41,15 @@ class App extends Component {
     this.setState(prevState => ({ balance: prevState.balance + params }));
   };
 
-  // updateCompleted = taskId => {
-  //   this.setState(prevState => {
-  //     return {
-  //       tasks: prevState.tasks.map(task => {
-  //         if (task.id === taskId) {
-  //           return {
-  //             ...task,
-  //             completed: !task.completed,
-  //           };
-  //         }
-
-  //         return task;
-  //       }),
-  //     };
-  //   });
-  // };
-
-  updateCompleted = taskId => {
-    this.setState(prevState => ({
-      tasks: prevState.tasks.map(task =>
-        task.id === taskId ? { ...task, completed: !task.completed } : task,
-      ),
-    }));
-  };
-
-  changeFilter = filter => {
-    this.setState({ filter: filter });
-  };
-
-  getVisibleTasks = () => {
-    const { tasks, filter } = this.state;
-
-    return tasks.filter(task =>
-      task.text.toLowerCase().includes(filter.toLowerCase()),
-    );
-  };
-
   render() {
-    const { tasks, balance, filter } = this.state;
-    const visibleTasks = this.getVisibleTasks();
+    const { balance } = this.state;
 
     return (
       <Layout>
-        <GlobalStyle />
         <Section title={'SHOP + COUNTER'}>
           <h2>balance: {balance}</h2>
           <Counter step={10} replenishment={this.replenishment} />
           <Product buyProduct={this.buyProduct} />
-        </Section>
-
-        <Section title={'TODO'}>
-          <TaskEditor onAddTask={this.addTask} />
-          {visibleTasks.length > 0 && (
-            <Filter value={filter} onChangeFilter={this.changeFilter} />
-          )}
-
-          {tasks.length > 0 && (
-            <TaskList
-              tasks={visibleTasks}
-              onRemoveTask={this.removeTask}
-              onUpdateTask={this.updateCompleted}
-            />
-          )}
         </Section>
 
         <Section title={'DROPDOWN'}>
